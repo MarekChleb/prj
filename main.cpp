0bb6f3faf6503b99fb176ad8a9841ff53efe5176
Marek <mw332637@students.mimuw.edu.pl>
2016-12-19 23:08:14 +0100

    first and last mb

--- main.cpp
+++ main.cpp
@@ -1,10 +1,97 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
+
+#define MAX_NK 100005
+#define MAX_M 500005
+#define MAX_P 100000009
 
 using namespace std;
 
+int N, M, K;
+bool DEBUG;
 
+vector<int> incoming[MAX_M];
+vector<int> outcoming[MAX_M];
+vector<pair<long long, int>> pro_needed_with_prj;
+long long pro_needed[MAX_NK];
+set<int> not_visited;
+int is_visited[MAX_NK];
+vector<long long> sorted_prj;
+
+void read_input() {
+    int left, right;
+    scanf("%d%d%d", &N, &M, &K);
+    for (int i = 0; i < N; i++) {
+        scanf("%lld", &pro_needed[i]);
+        pro_needed_with_prj.push_back(make_pair(pro_needed[i], i));
+    }
+    for (int i = 0; i < M; i++) {
+        scanf("%d%d", &left, &right);
+        //incoming[left].push_back(right);
+        outcoming[right].push_back(left);
+    }
+}
+
+void visit(int node) {
+    if (is_visited[node] == 0) {
+        for (int m : outcoming[node]) {
+            visit(m);
+        }
+        is_visited[node] = 1;
+        not_visited.erase(node);
+        sorted_prj.push_back(pro_needed[node]);
+    }
+}
+
+void topo_sort() {
+    sort(pro_needed_with_prj.begin(), pro_needed_with_prj.end());
+    for (int i = 1; i <= N; i++) {
+        not_visited.insert(pro_needed_with_prj[i].second);
+    }
+    while (!not_visited.empty()) {
+        visit(*(not_visited.begin()));
+    }
+    reverse(sorted_prj.begin(), sorted_prj.end());
+}
+
+/*void topo_sort() {
+    vector<int> no_inc_edges;
+    if (DEBUG) {
+        cout << "Nodes with no incoming edges: ";
+    }
+    for (int i = 1; i <= N; i++) {
+        if (incoming[i].empty()) {
+            if (DEBUG) {
+                cout << i << " ";
+            }
+            no_inc_edges.push_back(i);
+        }
+    }
+    if (DEBUG) {
+        cout << endl;
+    }
+
+    int n;
+    while (!no_inc_edges.empty()) {
+        n = no_inc_edges.back();
+        no_inc_edges.pop_back();
+        sorted_prj.push_back(n);
+        for (vector<int>::iterator m_it = outcoming[n].begin();
+             m_it < outcoming[n].end(); m_it++) {
+            outcoming[n].erase(m_it);
+            incoming[m]
+        }
+    }
+}*/
 
 int main() {
+
+    read_input();
+    topo_sort();
+    long long result = *(max_element(sorted_prj.begin() + N - K,
+                                     sorted_prj.end()));
+    cout << result;
     return 0;
 }
